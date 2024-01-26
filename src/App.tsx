a9a27624fbc722a4871264832029cc8fbbd154ce
Marcos Souza <marcos.souza@beeleads.com.br>
2024-01-26 12:45:29 -0300

    Instalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
 
 export function App() {
   return (
     <>
-      <h1>Hello React!</h1>
+      <div className="flex justify-center items-center h-screem">
+        <Button>Enviar</Button>
+      </div>
     </>
   )
 }
