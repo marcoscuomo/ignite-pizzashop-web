e31295d815bbe27adf550c6546ca87140962f548
Marcos Souza <marcos.souza@beeleads.com.br>
2024-01-29 18:50:14 -0300

    Layout do app com cabeçalho

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/header'
+
 export function AppLayout() {
   return (
-    <div>
-      <h1>Cabeçalho</h1>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
 
-      <div>
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        {/* flex flex-1 -> ocupará a largura inteira da tela, menos o espaço do cabeçalho */}
         <Outlet />
       </div>
     </div>
